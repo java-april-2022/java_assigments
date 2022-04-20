96c7d2a56a618d6717868fac8dd2944d059510de
jenricastro <97215501+jenricastro@users.noreply.github.com>
2022-04-20 03:09:47 -0400

    Add files via upload

---
+++ Week3/Extra Practice/DaikichiRoutes/DaikichiPathVariable/src/main/java/com/javastack/spring/daikichipathvariables/DaikichiPathVariableApplication.java
@@ -0,0 +1,13 @@
+package com.javastack.spring.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPathVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariableApplication.class, args);
+	}
+
+}
