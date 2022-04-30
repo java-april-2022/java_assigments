16e4ebafd7647aedf5a989fb9ead92713461de12
jenricastro <97215501+jenricastro@users.noreply.github.com>
2022-04-30 17:51:23 -0400

    Add files via upload

---
+++ Week4/Extra Practice/RenderingBooks/RenderingBooks/src/main/java/com/spring/renderingbooks/controllers/BookController.java
@@ -0,0 +1,24 @@
+package com.spring.renderingbooks.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.spring.renderingbooks.models.Book;
+import com.spring.renderingbooks.services.BookService;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping("/books/{id}")
+	public String show (@PathVariable ("id") Long id, Model model) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "show.jsp";
+	}
+}
